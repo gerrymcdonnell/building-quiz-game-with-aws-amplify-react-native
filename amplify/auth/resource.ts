f173547360c3356093ba652d7d068d11cee27a86
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2026-02-07 17:35:26 +0000

    amplify fixed

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,18 +1,11 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth } from '@aws-amplify/backend';
 
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
 export const auth = defineAuth({
   loginWith: {
-    email: {
-      verificationEmailSubject: "Welcome to quiz app! Verify your email!",
-      verificationEmailBody: (code) =>
-        `Here is your verification code: ${code()}`,
-      verificationEmailStyle: "CODE",
-    },
-  },
-  userAttributes: {
-    preferredUsername: {
-      mutable: true,
-      required: true,
-    },
+    email: true,
   },
 });
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,46 +1,17 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
+/*== STEP 1 ===============================================================
+The section below creates a Todo database table with a "content" field. Try
+adding a new "isDone" field as a boolean. The authorization rule below
+specifies that any unauthenticated user can "create", "read", "update", 
+and "delete" any "Todo" records.
+=========================================================================*/
 const schema = a.schema({
-  generateQuestions: a
-    .generation({
-      aiModel: a.ai.model("Claude 3.5 Sonnet"),
-      systemPrompt: `
-You are a quiz question generator.
-
-Create exactly 10 questions, evenly distributed across the categories from the following list [Sport, General Culture, Movies, Art, History]. Ensure the questions are evenly distributed in different difficulty levels.
-
-Requirements for each question:
-- The questions should be in English.
-- Return the result as a JSON list containing JSON objects.
-- Return the question with the JSON key 'question'.
-- Include 4 different answer options, with the JSON key 'options', each a string.
-- Specify 1 correct answer, with the JSON key 'correctAnswer', in string format.
-- Return the category with the JSON key 'category'.
-- The returned JSON will only have keys and values from the information from the mentioned before. Do not add any explanatory messages or statements such as 'Here is a JSON containing your trip', so user can take the JSON string and play around with it.
-- Questions should not be repeated.
-    `,
-    })
-    .arguments({
-      description: a.string(),
-    })
-    .returns(a.ref("Question").required().array().required())
-    .authorization((allow) => allow.authenticated()),
-  Question: a.customType({
-    question: a.string().required(),
-    options: a.string().required().array().required(),
-    correctAnswer: a.string().required(),
-    category: a.string().required(),
-  }),
-  Game: a
+  Todo: a
     .model({
-      playerOneId: a.string().required(),
-      playerTwoId: a.string().required(),
-      questions: a.ref("Question").required().array().required(),
-      currentQuestion: a.integer().default(0),
-      playerOneScore: a.integer().default(0),
-      playerTwoScore: a.integer().default(0),
+      content: a.string(),
     })
-    .authorization((allow) => [allow.authenticated()]),
+    .authorization((allow) => [allow.guest()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -48,6 +19,35 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "userPool",
+    defaultAuthorizationMode: 'identityPool',
   },
 });
+
+/*== STEP 2 ===============================================================
+Go to your frontend source code. From your client-side code, generate a
+Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
+WORK IN THE FRONTEND CODE FILE.)
+
+Using JavaScript or Next.js React Server Components, Middleware, Server 
+Actions or Pages Router? Review how to generate Data clients for those use
+cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
+=========================================================================*/
+
+/*
+"use client"
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+
+const client = generateClient<Schema>() // use this Data client for CRUDL requests
+*/
+
+/*== STEP 3 ===============================================================
+Fetch records from the database and use them in your frontend component.
+(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
+=========================================================================*/
+
+/* For example, in a React component, you can use this snippet in your
+  function's RETURN statement */
+// const { data: todos } = await client.models.Todo.list()
+
+// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
